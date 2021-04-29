9babdd4ea2d63257a78e177fc4801d1857b4d422
DESKTOP-LUKPNTP\josea <jose.alvarec@mail.udp.cl>
2021-04-28 20:20:52 -0400

    fin hello.py

---
+++ hello.py
@@ -0,0 +1,32 @@
+import pygame as pg 
+import sys
+
+def fin_juego():
+    pg.quit()
+    sys.exit()
+
+pg.init()
+pantalla = pg.display.set_mode((600, 400))
+pg.display.set_caption('Hola')
+
+game_over = False
+
+while not game_over:
+
+
+    #Gestion de eventos
+    for evento in pg.event.get():
+        if evento.type == pg.QUIT:
+            game_over = True
+        
+
+
+    #Gestion del estado
+    print ('hola mundo')
+
+    #Refrescar pantalla
+
+    pantalla.fill ((0, 255, 0))
+    pg.display.flip()
+
+fin_juego()
