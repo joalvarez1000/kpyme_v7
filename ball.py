21344db846117756611ef1061422c889f3a0dfde
DESKTOP-LUKPNTP\josea <jose.alvarec@mail.udp.cl>
2021-05-01 15:29:35 -0400

    creamos una raqueta y un pierdevida

--- ball.py
+++ ball.py
@@ -2,64 +2,61 @@ import pygame as pg
 import sys
 from random import randint, choice
 
-def rebotaX(x):
-    if x <=0 or x >=ANCHO:
-        return -1
-    return 1
-def rebotaY(y):
-    if y <=0 or y >=ALTO:
-        return -1
-    return 1
 
-#color bola
 ROJO = (255, 0, 0)
 AZUL = (0, 0, 255)
 VERDE = (0, 255, 0)
-
-#color pantalla
 NEGRO = (0, 0, 0)
-
-#tamaño pantalla
 ANCHO = 800
 ALTO = 600
-
-
-pg.init()#iniciar pantalla
+pg.init()
 pantalla = pg.display.set_mode((ANCHO, ALTO))
-reloj = pg.time.Clock() #tiempo de desplazamiento
+reloj = pg.time.Clock()
 
 class Bola():
-    def __init__(self, x, y, vx, vy, color): #Todas las clases de inicia con __init__ y se coloca self
+    def __init__(self, x, y, vx=5, vy=5, color= (255, 255, 255), radio=10):
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
         self.color = color
+        self.radio = radio
+    def actualizar(self):
+        self.x += self.vx
+        self.y += self.vy
+        if self.y <=0 or self.y >=ALTO:
+            self.vy = -self.vy
+        if self.x <=0 or self.x >=ANCHO:
+            self.vx = -self.vx
+    '''
+        bola.dibujar(pantalla)
+        #pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
+    '''
+    def dibujar(self, lienzo):
+        pg.draw.circle(lienzo, self.color, (self.x, self.y), self.radio)
 bolas = []
-for _ in range(10): #iteras 10 veces, para hacer 10 bolas
-    bola = Bola(randint(0, ANCHO), #ocupas la instancia de bola()
+for _ in range(10):
+    bola = Bola(randint(0, ANCHO),
                 randint(0, ALTO),
-                randint(5, 10)* choice([1, -1]),
-                randint(5, 10)* choice([1, -1]),
+                randint(5, 10)*choice([-1, 1]),
+                randint(5, 10)*choice([-1, 1]),
                 (randint(0, 255), randint(0,255), randint(0,255)))
-    bolas.append(bola) #Se agrega el resultado de la iteración
+    bolas.append(bola)
 game_over = False
 while not game_over:
-    v = reloj.tick(3)
+    v = reloj.tick(5)
     #Gestion de eventos
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
     # Modificación de estado
     for bola in bolas:
-        bola.x += bola.vx
-        bola.y += bola.vy
-        bola.vy *= rebotaY(bola.y)
-        bola.vx *= rebotaX(bola.x)
+        bola.actualizar()
     # Gestión de la pantalla
     pantalla.fill(NEGRO)
     for bola in bolas:
-        pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
+        bola.dibujar(pantalla)
+        #pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
     pg.display.flip()
 pg.quit()
 sys.exit()
